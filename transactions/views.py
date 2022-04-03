d871a7a4123315eaa57ba71d722bafea37048d88
unknown <mzitelli@itba.edu.ar>
2022-04-03 08:57:57 -0300

    Added first draft for inverse processing flow

--- transactions/views.py
+++ transactions/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from google.protobuf.json_format import MessageToJson
+from google.protobuf.json_format import Parse, ParseDict
 from transactions.v1_payments_request_pb2 import v1_payments_request
 from transactions.ROSRequest_pb2 import ROSRequest
 from transactions.ROSEnums_pb2 import Method
@@ -10,6 +11,9 @@ from transactions.HTTPHeaders_pb2 import Headers
 import json
 import base64
 import zlib
+import requests
+from twilio.rest import Client
+from django.conf import settings
 
 # Create your views here.
 
@@ -17,6 +21,8 @@ import zlib
 class ROSView(APIView):
     def get(self, request):
         try:
+            from_number = request.query_params.get('From', "")
+
             print('Starting processing pipeline...')
             message = request.query_params.get('Body', "")
             message = message.replace(' ', '+')
@@ -49,6 +55,48 @@ class ROSView(APIView):
                 print()
                 print("BODY:")
                 print(body_received)
+
+            if ros_received.uri == 'https://be.buenbit.com/api/market/tickers/' and ros_received.method == Method.GET:
+                # body = v1_payments_request()
+                # body.ParseFromString(ros_received.body)
+                # body_received = MessageToJson(body)
+                #
+                # print("HEADERS:")
+                # print(headers_received)
+                # print()
+                # print("BODY:")
+                # print(body_received)
+
+                r = request.get(ros_received.uri)
+                body_tosend = r.json()
+
+                body = ParseDict(body_tosend, v1_payments_request()).SerializeToString()
+
+                ros = ROSRequest()
+                ros.method = ros_received.method
+                ros.uri = ros_received.uri
+                ros.headers = headers
+                ros.body = body
+
+                ros_plain = f"{ros}"
+                ros_str = ros.SerializeToString()
+
+                body_compressed = zlib.compress(ros_str, level=9)
+                plain_compressed = zlib.compress(str.encode(ros_plain), level=9)
+
+                body_encoded = base64.b64encode(body_compressed)
+                plain_encoded = base64.b64encode(plain_compressed)
+
+                account_sid = settings.TWILIO_ACCOUNT_ID
+                auth_token = settings.TWILIO_AUTH_TOKEN
+                client = Client(account_sid, auth_token)
+
+                message = client.messages.create(
+                    body=f'{body_encoded}',
+                    from_='+16064044957',
+                    to=from_number
+                )
+
             return Response(data="ROS successful request", status=200)
         except Exception as e:
             print(e)
