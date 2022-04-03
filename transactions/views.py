c4a28a3fd3c3f5c1474822e21085efeea0452c27
unknown <mzitelli@itba.edu.ar>
2022-04-03 06:23:48 -0300

    Added protobuf messages and direct processing flow

--- transactions/views.py
+++ transactions/views.py
@@ -2,13 +2,48 @@ from django.http import HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from google.protobuf.json_format import MessageToJson
+from v1_payments_request_pb2 import v1_payments_request
+from ROSRequest_pb2 import ROSRequest
+from ROSEnums_pb2 import Method
+from HTTPHeaders_pb2 import Headers
+import json
+import base64
+import zlib
+
 # Create your views here.
 
 
 class ROSView(APIView):
     def get(self, request):
         try:
-            print(request.query_params)
+            message = request.query_params.get('Body', "")
+
+            # Decode
+            body_compressed = base64.b64decode(message)
+
+            # Decompress
+            body = zlib.decompress(body_compressed)
+
+            # Parse protobuf
+            ros_received = ROSRequest()
+            ros_received.ParseFromString(body)
+            # ros_message_received = MessageToJson(ros_received)
+
+            if ros_received.uri == "https://api.mercadopago.com/v1/payments" and ros_received.method == Method.GET:
+                headers = Headers()
+                headers.ParseFromString(ros_received.headers)
+                headers_received = MessageToJson(headers)
+
+                body = v1_payments_request()
+                body.ParseFromString(ros_received.body)
+                body_received = MessageToJson(body)
+
+                print("HEADERS:")
+                print(headers_received)
+                print()
+                print("BODY:")
+                print(body_received)
             return Response(data="ROS successful request", status=200)
         except Exception as e:
             print(e)
