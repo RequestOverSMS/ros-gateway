eb5b464ed31f79d5e219f69d0998e1dfe356c102
unknown <mzitelli@itba.edu.ar>
2022-04-03 04:55:56 -0300

    Added error message

--- transactions/views.py
+++ transactions/views.py
@@ -9,7 +9,8 @@ class ROSView(APIView):
     def get(self, request):
         try:
             print(request.query_params)
-            return Response(data="ROS request", status=200)
+            return Response(data="ROS successful request", status=200)
         except Exception as e:
-            return HttpResponse('Bad request', status=404)
+            print(e)
+            return HttpResponse('ROS Bad request', status=400)
 
