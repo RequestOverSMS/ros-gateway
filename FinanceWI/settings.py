ddc6307d8568e111fd0176b18432f8fa1f2e39f1
unknown <mzitelli@itba.edu.ar>
2022-04-03 03:53:16 -0300

    Added dotenv

--- FinanceWI/settings.py
+++ FinanceWI/settings.py
@@ -11,19 +11,27 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", __file__)
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+os.environ['BASE_DIR'] = BASE_DIR
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)x!^y$(zkl0#l=7j4g4526wiz-)zhc-u$svs(uukrn-+ydgv&w'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
